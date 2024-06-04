ed6d8bb3b5fea130f9fdef7dd32eb2eb1d1aa193
hasanraza38 <92hasanraza689@gmail.com>
2024-06-05 03:10:35 +0500

    upates

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const div = document.querySelector('#cards-render')
 
+let products ='';
 axios('https://fakestoreapi.com/products')
 .then((res)=>{
-console.log(res.data);
-res.data.map((item)=>{
-    div.innerHTML+=`
+  // console.log(res.data);
+products = res.data
+products.map((item)=>{
+
+    div.innerHTML +=
+  
+    `
     <div class="card" style="width: 18rem;">
     <img width="200px" height="250px" src="${item.image}" class="card-img-top" alt="image">
     <div class="card-body">
       <h5 class="card-title">${item.title}</h5>
       <h6 class="card-text">${item.price}<b>$</b></h6>
-      <a href="#" class="btn btn-primary">Order now</a>
+      <button class="btn btn-primary">order now</button>
     </div>
   </div>
     
@@ -18,5 +23,46 @@ res.data.map((item)=>{
 })
 })
 .catch((err)=>{
-    console.log(err.data);
-})
+    console.error(err.data);
+});
+
+const allItems=()=>{
+
+  div.innerHTML=""
+  products.map((item)=>{
+    div.innerHTML +=
+    `
+    <div class="card" style="width: 18rem;">
+    <img width="200px" height="250px" src="${item.image}" class="card-img-top" alt="image">
+    <div class="card-body">
+      <h5 class="card-title">${item.title}</h5>
+      <h6 class="card-text">${item.price}<b>$</b></h6>
+      <button class="btn btn-primary">order now</button>
+    </div>
+  </div>
+    
+    `
+  })
+
+}
+
+
+const itemFilter=(btn)=>{
+
+ div.innerHTML="" 
+//  console.log(btn.innerHTML);
+ const filtred = products.filter(items=>items.category=== btn.innerHTML).map((item)=>{
+
+  div.innerHTML+=`
+  <div class="card" style="width: 18rem;">
+    <img width="200px" height="250px" src="${item.image}" class="card-img-top" alt="image">
+    <div class="card-body">
+      <h5 class="card-title">${item.title}</h5>
+      <h6 class="card-text">${item.price}<b>$</b></h6>
+      <button class="btn btn-primary">order now</button>
+    </div>
+  </div>`
+ })
+
+
+}
